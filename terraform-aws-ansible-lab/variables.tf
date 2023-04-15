31daf66f5bc5ee26cc1e2b290d61a3a5b9974db9
Robert Lilly <robert@rclillyit.com>
2023-04-15 13:56:13 -0700

    Change AMI ID and ssh key name

--- terraform-aws-ansible-lab/variables.tf
+++ terraform-aws-ansible-lab/variables.tf
@@ -1,17 +1,15 @@
 variable "aws_ami_id" {
   ## Amazon Linux 2 AMI (HVM)
-  default = "ami-02f26adf094f51167"
+  default = "ami-0747e613a2a1ff483"
   ## "ami-0cd31be676780afa7"
 }
 
 variable "ssh_key_pair" {
-  default = "~/.ssh/id_rsa"
-  #default = "~/.ssh/id_rsa_ansilble_lab"
+  default = "~/.ssh/learn_rsa"
 }
 
 variable "ssh_key_pair_pub" {
-  default = "~/.ssh/id_rsa.pub"
-  #default = "~/.ssh/id_rsa_ansilble_lab.pub"
+  default = "~/.ssh/learn_rsa.pub"
 }
 
 variable "ansible_node_count" {
